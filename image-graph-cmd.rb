6f3ade42b086bd733ab18e355459dae8bb9810eb
Bryon Roche <bryon.roche@rd.io>
2014-12-15 18:33:52 -0800

    Show image sizes, and for non-tagged images, show just the size diff between its parent to guess the ondisk usage.

--- image-graph-cmd.rb
+++ image-graph-cmd.rb
@@ -2,6 +2,10 @@
 require 'open3'
 require 'docker'
 
+sizelists = Hash.new()
+childlists = Hash.new() { |h,k| h[k]=[] }
+parents = {}
+
 dot_file = []
 dot_file << 'digraph docker {'
 
@@ -10,6 +14,9 @@ Docker::Image.all(all: true).each do |image|
   id = image.id[0..11]
   tags = image.info['RepoTags'].reject { |t| t == '<none>:<none>' }.join('\n')
   parent_id = image.info['ParentId'][0..11]
+  size = image.info["VirtualSize"]
+  sizelists[id.to_sym] = size
+  parents[id] = parent_id
 
   if parent_id.empty?
     dot_file << "base -> \"#{id}\" [style=invis]"
@@ -17,16 +24,38 @@ Docker::Image.all(all: true).each do |image|
     dot_file << "\"#{parent_id}\" -> \"#{id}\""
   end
 
-  unless tags.empty?
-    dot_file << "\"#{id}\" [label=\"#{id}\\n#{tags}\",shape=box,fillcolor=\"paleturquoise\",style=\"filled,rounded\"];"
+  childlists[parent_id].push id
+
+  if !tags.empty?
+    dot_file << "\"#{id}\" [label=\"#{id}\\n#{tags}\\n%{#{id}}MiB\n(Virtual: #{size / 1024.0}MiB)\",shape=box,fillcolor=\"paleturquoise\",style=\"filled,rounded\"];"
+  else
+    dot_file << "\"#{id}\" [label=\"#{id}\\n%{#{id}}MiB\"];"
   end
+
 end
 
 dot_file << 'base [style=invisible]'
 dot_file << '}'
 
-Open3.popen3('/usr/bin/dot -Tpng') do |stdin, stdout, stderr|
-  stdin.puts(dot_file.join("\n"))
+parents.keys.each { |k| childlists[k] } #iterate to make sure we have empty children
+
+until childlists.empty?
+  childlists.delete_if do |k, v|
+    if v.empty?
+      sizelists[k.to_sym] = ( sizelists[k.to_sym] - sizelists[parents[k].to_sym] ) / 1024.0 rescue 0
+      childlists.each do |rk,rv|
+        rv.delete(k)
+      end
+    end
+    v.empty?
+  end
+end
+
+dot_file = dot_file.join("\n") % sizelists
+
+Open3.popen3('/usr/bin/dot -Tsvg') do |stdin, stdout, stderr|
+  stdin.puts(dot_file)
   stdin.close
   STDOUT.write stdout.read
+  STDERR.write stderr.read
 end
